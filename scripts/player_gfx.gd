6ee73d88dfc788dc193fb570d40f95b251fa773f
APirateHat <89027061+APirateHat@users.noreply.github.com>
2024-11-09 15:55:21 +0100

    Graphics (#4)
    * Inital graphics commit
    
    * Added background and player graphics
    
    * Added new folders, main menu and player sprite script
    
    ---------
    
    Co-authored-by: Floris Vastenhout <kilmanio@mailfence.com>

---
+++ scripts/player_gfx.gd
@@ -0,0 +1,42 @@
+extends Node2D
+
+var rotation_value = 0
+var movement_value = 0
+
+func _ready() -> void:
+	shader_param(0.0)
+
+func _process(delta: float) -> void:
+	lerp_rotation(movement_value)
+
+func change_sprite(val):
+	match val:
+		0:
+			$Alien.play("idle")
+			rotation_value = 0
+		1:
+			$Alien.play("turn_right")
+			$Alien.flip_h = false
+			rotation_value = 25
+		-1:
+			$Alien.play("turn_right")
+			$Alien.flip_h = true
+			rotation_value = -35
+
+func tractor():
+	$AnimationPlayer.play("tractor_bounce")
+	var tween = get_tree().create_tween()
+	tween.tween_method(shader_param, 0.0, 1.0, 0.2).set_trans(Tween.TRANS_CUBIC)
+	tween.tween_callback(tween.kill)
+
+func detractor():
+	var tween = get_tree().create_tween()
+	tween.tween_method(shader_param, 1.0, 0, 0.1)
+	tween.tween_callback(tween.kill)
+
+func shader_param(value:float):
+	$TractorBeam.material.set("shader_parameter/dissolve_value", value)
+
+func lerp_rotation(value:int=0):
+	change_sprite(value)
+	rotation = lerp_angle(rotation, deg_to_rad(rotation_value), 0.1)
