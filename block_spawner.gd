0cdcd70158f65871c98c8cff3cadd7e33a6c487a
Floris Vastenhout <kilmanio@mailfence.com>
2024-11-09 16:59:01 +0100

    Add block_spawner scene

---
+++ block_spawner.gd
@@ -0,0 +1,23 @@
+extends Node2D
+
+var scene_to_instance = preload("res://block.tscn")
+var timer = 5
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	if $Area2D.has_overlapping_bodies():
+		timer = 5
+	else:
+		timer -= delta
+		
+	if timer <= 0:
+		spawn_block()
+
+func spawn_block():
+	var object = scene_to_instance.instantiate()
+	object.position = position
+	call_deferred("add_child",object)
--- game.gd
+++ game.gd
@@ -1,24 +1,9 @@
 extends Node2D
 
-var scene_to_instance = preload("res://block.tscn")
-var timeout = 0
-
-
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	timeout -= delta
-	if Input.is_action_pressed("ui_text_toggle_insert_mode") && timeout <= 0:
-		spawn_block()
-	
-
-
-func spawn_block():
-	var object = scene_to_instance.instantiate()
-	object.position = $ship.position
-	object.position.y += 100
-	call_deferred("add_child",object)
-	timeout = 1
+	pass
