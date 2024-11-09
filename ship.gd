129c37b46af6f543cc7282c1379a11cad1bee481
Floris Vastenhout <kilmanio@mailfence.com>
2024-11-09 13:33:40 +0100

    Change player to be a rigidbody with force

--- ship.gd
+++ ship.gd
@@ -1,9 +1,19 @@
-extends CharacterBody2D
-
+extends RigidBody2D
+const SHIP_SPEED = 300
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	lock_rotation = true
+	contact_monitor = true
+	max_contacts_reported = 1
+	set_continuous_collision_detection_mode(2)
+	set_gravity_scale(0)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
-	move_and_slide()
+	match [Input.is_action_pressed("ui_left"), Input.is_action_pressed("ui_right")]:
+		[true, false]: apply_force(Vector2(-SHIP_SPEED, 0))
+		[false, true]: apply_force(Vector2(SHIP_SPEED, 0))
+	
+	match [Input.is_action_pressed("ui_down"), Input.is_action_pressed("ui_up")]:
+		[true, false]: apply_force(Vector2(0, SHIP_SPEED))
+		[false, true]: apply_force(Vector2(0, -SHIP_SPEED))
