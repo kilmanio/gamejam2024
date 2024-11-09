de0192f7e91fb4705603b22446c75e7f206981ad
Floris Vastenhout <kilmanio@mailfence.com>
2024-11-09 13:18:41 +0100

    Git init + basic things

---
+++ ship.gd
@@ -0,0 +1,9 @@
+extends CharacterBody2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
+	move_and_slide()
